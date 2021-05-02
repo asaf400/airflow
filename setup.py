cd0f9d987baf958905a4abb638b822150c3caa35
asaf.levy <asaf.levy@ironsrc.com>
2021-05-03 01:12:33 +0300

    sql

--- setup.py
+++ setup.py
@@ -201,7 +201,8 @@ cassandra = [
     'cassandra-driver>=3.13.0,<3.21.0',
 ]
 celery = [
-    'celery~=4.3',
+    'vine==1.3.0',
+    'celery==4.3.1',
     'flower>=0.7.3, <1.0',
     'kombu==4.6.3;python_version<"3.0"',
     'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
